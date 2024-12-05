6468c6b68f9e43dc38d532137bab34b080d28470
Shubham Singh <mine.shubhamsingh@gmail.com>
2024-12-05 21:49:46 +0530

    beggining of Jarvis

--- Jarvis-Project/main.py
+++ Jarvis-Project/main.py
@@ -1,12 +1,12 @@
 import speech_recognition as sr
-import webbrowser
 import pyttsx3
 
-recogniser = sr.Recognizer()
+recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
-  engine.say("I will speak this text")
+    engine.say(text)
+    engine.runAndWait()
 
-if __name__ == '__main__':
-  speak("Hey sir how may i help you")
+if __name__ == "__main__":
+    speak("Yes Boss? How can I help you?")
