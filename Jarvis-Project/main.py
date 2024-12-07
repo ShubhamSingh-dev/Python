d0276e2adcfce47202b5ac8fa4690d6848fb60d1
Shubham Singh <mine.shubhamsingh@gmail.com>
2024-12-07 22:27:33 +0530

    Jarvis Updation

--- Jarvis-Project/main.py
+++ Jarvis-Project/main.py
@@ -1,37 +1,92 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
-import musicLibrary 
+import musicLibrary
+import requests
+# from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
+
+# pip install pocketsphinx
 
 recognizer = sr.Recognizer()
-engine = pyttsx3.init()
+engine = pyttsx3.init() 
+# newsapi = "<Your Key Here>"
 
-def speak(text):
+def speak_old(text):
     engine.say(text)
     engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
+
+# def aiProcess(command):
+#     client = OpenAI(api_key="<Your Key Here>",
+#     )
+
+#     completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+#         {"role": "user", "content": command}
+#     ]
+#     )
+
+#     return completion.choices[0].message.content
 
 def processCommand(c):
     if "open google" in c.lower():
         webbrowser.open("https://google.com")
-        speak("opening google")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
     elif "open youtube" in c.lower():
         webbrowser.open("https://youtube.com")
-        speak("opening youtube")
     elif "open linkedin" in c.lower():
         webbrowser.open("https://linkedin.com")
-        speak("opening linkedin")
-    elif "open instagram" in c.lower():
-        webbrowser.open("https://instagram.com")
-        speak("opening instagram")
     elif c.lower().startswith("play"):
         song = c.lower().split(" ")[1]
-        link = musicLibrary.music(song)
+        link = musicLibrary.music[song]
         webbrowser.open(link)
 
-    elif "news" in c.lower():
-        webbrowser.open("https://www.bbc.com/news")
-        speak("opening news")
-    
+    # elif "news" in c.lower():
+    #     r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}")
+    #     if r.status_code == 200:
+    #         # Parse the JSON response
+    #         data = r.json()
+            
+    #         # Extract the articles
+    #         articles = data.get('articles', [])
+            
+    #         # Print the headlines
+    #         for article in articles:
+    #             speak(article['title'])
+
+    # else:
+    #     # Let OpenAI handle the request
+    #     output = aiProcess(c)
+    #     speak(output) 
+
+
+
+
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
@@ -39,15 +94,16 @@ if __name__ == "__main__":
         # Listen for the wake word "Jarvis"
         # obtain audio from the microphone
         r = sr.Recognizer()
-
-        print("Recognizing...")
+         
+        print("recognizing...")
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source,timeout=2,phrase_time_limit=1)
-            word = r.recognize_google(audio)   
-            if (word.lower() == "jarvis"):
-                speak("Yes sir")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                # Listen for command
                 with sr.Microphone() as source:
                     print("Jarvis Active...")
                     audio = r.listen(source)
@@ -55,9 +111,8 @@ if __name__ == "__main__":
 
                     processCommand(command)
 
-        except Exception as e:
-            speak("Sorry, I didn't catch that. Please try again.")
-            print("Error; {0}".format(e))
 
+        except Exception as e:
+            print("Error; {0}".format(e))
 
 
